6d82635ed5ff29294679deb9ab043b5203b7323b
Maneesha Dulshanjan <83775004+Maneeha1234SL@users.noreply.github.com>
2023-10-27 18:59:39 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,123 @@
-# Linux-Optimizer
+# Linux Optimizer
+
+## This Bash script automates the optimization of your Linux server.
+### It performs the following tasks:
+       
+0. Fix `hosts` file and DNS _(temporarily)_:
+    - Check and add 127.0.1.1 and server hostname to `/etc/hosts`.
+    
+    *Original `hosts` file is backed up at `/etc/hosts.bak`.*
+    - Add `1.1.1.1`, `1.0.0.1`, `8.8.8.8` and `8.8.4.4` to `/etc/resolv.conf`.
+    
+    *Original `dns` file is backed up at `/etc/resolv.conf.bak`.*
+
+
+1. Update, Upgrade, and Clean the server:
+    - _Update_
+    - _Upgrade_
+    - _Full-Upgrade_
+    - _AutoRemove_
+    - _AutoClean_
+    - _Clean_
+
+
+2. Install XanMod LTS Kernel _(Only on Ubuntu & Debian)_:
+    - Enable BBRv3.
+    - CloudFlare TCP Optimizations.
+    - More Details: https://xanmod.org
+
+
+3. Install Useful Packages:
+
+    _`apt-transport-https`_ _`apt-utils`_ _`autoconf`_ _`automake`_ _`bash-completion`_ _`bc`_ _`binutils`_ _`binutils-common`_ _`binutils-x86-64-linux-gnu`_ _`build-essential`_ _`busybox`_ _`ca-certificates`_ _`cron`_ _`curl`_ _`dialog`_ _`epel-release`_ _`gnupg2`_ _`git`_ _`haveged`_ _`htop`_ _`iptables`_ _`iptables-persistent`_ _`jq`_ _`keyring`_ _`libssl-dev`_ _`libsqlite3-dev`_ _`libtool`_ _`locales`_ _`lsb-release`_ _`make`_ _`nano`_ _`net-tools`_ _`nftables`_ _`packagekit`_ _`preload`_ _`python3`_ _`python3-pip`_ _`qrencode`_ _`socat`_ _`screen`_ _`software-properties-common`_ _`ufw`_ _`unzip`_ _`vim`_ _`wget`_ _`zip`_
+
+
+4. Enable Packages at Server Boot.
+
+    
+5. Set the server TimeZone to the VPS IP address location.
+
+ 
+6. Create & Enable `SWAP` File:
+    - Swap Path: `"/swapfile"`
+    - Swap Size: `2Gb`
+
+
+7. Enable `IPv6` Support.
+
+
+8. Optimize the [SYSCTL](https://github.com/hawshemi/Linux-Optimizer/blob/main/files/sysctl.conf) Configs:
+    - Optimize `SWAP`.
+    - Optimize Network Settings.
+    - Activate `BBR` _(`BBRv3` with XanMod)_.
+    - Optimize the Kernel.
+
+    *Original file is backed up at `/etc/sysctl.conf.bak`.*
+
+    
+9. Optimize [SSH](https://github.com/hawshemi/Linux-Optimizer/blob/main/files/sshd_config):
+    - Disable DNS lookups for connecting clients.
+    - Remove less efficient encryption ciphers.
+    - Enable and Configure TCP keep-alive messages.
+    - Allow agent & TCP forwarding.
+    - Enable gateway ports, Tunneling and compression.
+    - Enable X11 Forwarding.
+
+    *Original file is backed up at `/etc/ssh/sshd_config.bak`.*
+   
+
+10. Optimize the [System Limits](https://github.com/hawshemi/Linux-Optimizer/blob/main/files/profile):
+    - Soft and Hard ulimit `-c -d -f -i -l -n -q -s -u -v -x` optimizations.
+    
+    
+11. Optimize `UFW` and open Common Ports:
+    - Open Ports `SSH`, `80`, `443`.
+    - With `IPv6`, `TCP` & `UDP`.
+
+    
+Reboot at the end is Recommended.
+
+
+## Prerequisites
+
+### Ensure that the `sudo`, `wget` and `jq` packages are installed on your system:
+
+- Ubuntu & Debian:
+```
+apt update -q && apt install -y sudo wget jq
+```
+- CentOS & Fedora:
+```
+dnf up -y && dnf install -y sudo wget jq
+```
+
+
+## Run
+#### **Tested on:** Ubuntu 20+, Debian 11+, CentOS Stream 8+, AlmaLinux 8+, Fedora 37+
+
+#### Root Access is Required. If the user is not root, first run:
+```
+sudo -i
+```
+#### Then:
+```
+wget "https://raw.githubusercontent.com/hawshemi/Linux-Optimizer/main/linux-optimizer.sh" -O linux-optimizer.sh && chmod +x linux-optimizer.sh && bash linux-optimizer.sh 
+```
+
+
+## Menu Image
+### Debian & Ubuntu:
+![debian-based-menu](https://github.com/hawshemi/Linux-Optimizer/assets/16742123/3604470e-48ed-403d-a753-143dc934f6fd)
+
+### CentOS, AlmaLinux & Fedora:
+![rhel-based-menu](https://github.com/hawshemi/Linux-Optimizer/assets/16742123/07099e64-3864-425f-83e2-cda1e57d5b62)
+
+
+
+## Disclaimer
+This script is provided as-is, without any warranty or guarantee. Use it at your own risk.
+
+
+## License
+This script is licensed under the MIT License.
+
---
+++ scripts/centos-optimizer.sh
@@ -0,0 +1,646 @@
+#!/bin/bash
+# https://github.com/hawshemi/Linux-Optimizer
+
+
+# Green, Yellow & Red Messages.
+green_msg() {
+    tput setaf 2
+    echo "[*] ----- $1"
+    tput sgr0
+}
+
+yellow_msg() {
+    tput setaf 3
+    echo "[*] ----- $1"
+    tput sgr0
+}
+
+red_msg() {
+    tput setaf 1
+    echo "[*] ----- $1"
+    tput sgr0
+}
+
+
+# Declare Paths & Settings.
+SYS_PATH="/etc/sysctl.conf"
+PROF_PATH="/etc/profile"
+SSH_PORT=""
+SSH_PATH="/etc/ssh/sshd_config"
+SWAP_PATH="/swapfile"
+SWAP_SIZE=2G
+
+
+# Root
+check_if_running_as_root() {
+    ## If you want to run as another user, please modify $EUID to be owned by this user
+    if [[ "$EUID" -ne '0' ]]; then
+      echo 
+      red_msg 'Error: You must run this script as root!'
+      echo 
+      sleep 0.5
+      exit 1
+    fi
+}
+
+
+# Check Root
+check_if_running_as_root
+sleep 0.5
+
+
+# Ask Reboot
+ask_reboot() {
+    yellow_msg 'Reboot now? (RECOMMENDED) (y/n)'
+    echo 
+    while true; do
+        read choice
+        echo 
+        if [[ "$choice" == 'y' || "$choice" == 'Y' ]]; then
+            sleep 0.5
+            reboot
+            exit 0
+        fi
+        if [[ "$choice" == 'n' || "$choice" == 'N' ]]; then
+            break
+        fi
+    done
+}
+
+
+# Update & Upgrade & Remove & Clean
+complete_update() {
+    echo 
+    yellow_msg 'Updating the System... (This can take a while.)'
+    echo 
+    sleep 0.5
+
+    sudo dnf -y up
+    sudo dnf -y autoremove
+    sudo dnf -y clean all
+    sleep 0.5
+    
+    ## Again :D
+    sudo dnf -y up
+    sudo dnf -y autoremove
+    
+    echo 
+    green_msg 'System Updated & Cleaned Successfully.'
+    echo 
+    sleep 0.5
+}
+
+
+# Install useful packages
+installations() {
+    echo 
+    yellow_msg 'Installing Useful Packages... (This can take a while.)'
+    echo 
+    sleep 0.5
+
+    ## Install EPEL repository
+    sudo dnf -y install epel-release
+
+    ## Update for the EPEL
+    sudo dnf -y up
+
+    ## Networking packages
+    sudo dnf -y install iptables iptables-services nftables
+
+    ## System utilities
+    sudo dnf -y install bash-completion ca-certificates crontabs curl dnf-plugins-core dnf-utils gnupg2 nano screen ufw unzip vim wget zip
+
+    ## Programming and development tools
+    sudo dnf -y install autoconf automake bash-completion git libtool make pkg-config python3 python3-pip
+
+    ## Additional libraries and dependencies
+    sudo dnf -y install bc binutils haveged jq libsodium libsodium-devel PackageKit qrencode socat
+
+    ## Miscellaneous
+    sudo dnf -y install dialog htop net-tools
+
+    echo 
+    green_msg 'Useful Packages Installed Succesfully.'
+    echo 
+    sleep 0.5
+}
+
+
+# Enable packages at server boot
+enable_packages() {
+    sudo systemctl enable crond.service haveged nftables
+    echo 
+    green_msg 'Packages Enabled Succesfully.'
+    echo
+    sleep 0.5
+}
+
+
+## Swap Maker
+swap_maker() {
+    echo 
+    yellow_msg 'Making SWAP Space...'
+    echo 
+    sleep 0.5
+
+    ## Make Swap
+    sudo fallocate -l $SWAP_SIZE $SWAP_PATH  ## Allocate size
+    sudo chmod 600 $SWAP_PATH                ## Set proper permission
+    sudo mkswap $SWAP_PATH                   ## Setup swap         
+    sudo swapon $SWAP_PATH                   ## Enable swap
+    echo "$SWAP_PATH   none    swap    sw    0   0" >> /etc/fstab ## Add to fstab
+
+    echo 
+    green_msg 'SWAP Created Successfully.'
+    echo
+    sleep 0.5
+}
+
+
+# SYSCTL Optimization
+sysctl_optimizations() {
+    ## Make a backup of the original sysctl.conf file
+    cp $SYS_PATH /etc/sysctl.conf.bak
+
+    echo 
+    yellow_msg 'Default sysctl.conf file Saved. Directory: /etc/sysctl.conf.bak'
+    echo 
+    sleep 1
+
+    echo 
+    yellow_msg 'Optimizing the Network...'
+    echo 
+    sleep 0.5
+
+    ## Replace the new sysctl.conf file.
+    wget "https://raw.githubusercontent.com/hawshemi/Linux-Optimizer/main/files/sysctl.conf" -q -O $SYS_PATH 
+
+    sysctl -p
+
+    echo 
+    green_msg 'Network is Optimized.'
+    echo 
+    sleep 0.5
+}
+
+
+# Function to find the SSH port and set it in the SSH_PORT variable
+find_ssh_port() {
+    echo 
+    yellow_msg "Finding SSH port..."
+    echo 
+
+    ## Check if the SSH configuration file exists
+    if [ -e "$SSH_PATH" ]; then
+        ## Use grep to search for the 'Port' directive in the SSH configuration file
+        SSH_PORT=$(grep -oP '^Port\s+\K\d+' "$SSH_PATH" 2>/dev/null)
+
+        if [ -n "$SSH_PORT" ]; then
+            echo 
+            green_msg "SSH port found: $SSH_PORT"
+            echo 
+            sleep 0.5
+        else
+            echo 
+            green_msg "SSH port is default 22."
+            echo 
+            SSH_PORT=22
+            sleep 0.5
+        fi
+    else
+        red_msg "SSH configuration file not found at $SSH_PATH"
+    fi
+}
+
+
+# Remove old SSH config to prevent duplicates.
+remove_old_ssh_conf() {
+    ## Make a backup of the original sshd_config file
+    cp $SSH_PATH /etc/ssh/sshd_config.bak
+
+    echo 
+    yellow_msg 'Default SSH Config file Saved. Directory: /etc/ssh/sshd_config.bak'
+    echo 
+    sleep 1
+    
+    ## Disable DNS lookups for connecting clients
+    sed -i 's/#UseDNS yes/UseDNS no/' $SSH_PATH
+
+    ## Enable compression for SSH connections
+    sed -i 's/#Compression no/Compression yes/' $SSH_PATH
+
+    ## Remove less efficient encryption ciphers
+    sed -i 's/Ciphers .*/Ciphers aes256-ctr,chacha20-poly1305@openssh.com/' $SSH_PATH
+
+    ## Remove these lines
+    sed -i '/MaxAuthTries/d' $SSH_PATH
+    sed -i '/MaxSessions/d' $SSH_PATH
+    sed -i '/TCPKeepAlive/d' $SSH_PATH
+    sed -i '/ClientAliveInterval/d' $SSH_PATH
+    sed -i '/ClientAliveCountMax/d' $SSH_PATH
+    sed -i '/AllowAgentForwarding/d' $SSH_PATH
+    sed -i '/AllowTcpForwarding/d' $SSH_PATH
+    sed -i '/GatewayPorts/d' $SSH_PATH
+    sed -i '/PermitTunnel/d' $SSH_PATH
+    sed -i '/X11Forwarding/d' $SSH_PATH
+}
+
+
+# Update SSH config
+update_sshd_conf() {
+    echo 
+    yellow_msg 'Optimizing SSH...'
+    echo 
+    sleep 0.5
+
+    ## Enable TCP keep-alive messages
+    echo "TCPKeepAlive yes" | tee -a $SSH_PATH
+
+    ## Configure client keep-alive messages
+    echo "ClientAliveInterval 3000" | tee -a $SSH_PATH
+    echo "ClientAliveCountMax 100" | tee -a $SSH_PATH
+
+    ## Allow agent forwarding
+    echo "AllowAgentForwarding yes" | tee -a $SSH_PATH
+
+    ## Allow TCP forwarding
+    echo "AllowTcpForwarding yes" | tee -a $SSH_PATH
+
+    ## Enable gateway ports
+    echo "GatewayPorts yes" | tee -a $SSH_PATH
+
+    ## Enable tunneling
+    echo "PermitTunnel yes" | tee -a $SSH_PATH
+
+    ## Enable X11 graphical interface forwarding
+    echo "X11Forwarding yes" | tee -a $SSH_PATH
+
+    ## Restart the SSH service to apply the changes
+    systemctl restart sshd
+
+    echo 
+    green_msg 'SSH is Optimized.'
+    echo 
+    sleep 0.5
+}
+
+
+# System Limits Optimizations
+limits_optimizations() {
+    echo
+    yellow_msg 'Optimizing System Limits...'
+    echo 
+    sleep 0.5
+
+    ## Clear old ulimits
+    sed -i '/ulimit -c/d' $PROF_PATH
+    sed -i '/ulimit -d/d' $PROF_PATH
+    sed -i '/ulimit -f/d' $PROF_PATH
+    sed -i '/ulimit -i/d' $PROF_PATH
+    sed -i '/ulimit -l/d' $PROF_PATH
+    sed -i '/ulimit -m/d' $PROF_PATH
+    sed -i '/ulimit -n/d' $PROF_PATH
+    sed -i '/ulimit -q/d' $PROF_PATH
+    sed -i '/ulimit -s/d' $PROF_PATH
+    sed -i '/ulimit -t/d' $PROF_PATH
+    sed -i '/ulimit -u/d' $PROF_PATH
+    sed -i '/ulimit -v/d' $PROF_PATH
+    sed -i '/ulimit -x/d' $PROF_PATH
+    sed -i '/ulimit -s/d' $PROF_PATH
+
+
+    ## Add new ulimits
+    ## The maximum size of core files created.
+    echo "ulimit -c unlimited" | tee -a $PROF_PATH
+
+    ## The maximum size of a process's data segment
+    echo "ulimit -d unlimited" | tee -a $PROF_PATH
+
+    ## The maximum size of files created by the shell (default option)
+    echo "ulimit -f unlimited" | tee -a $PROF_PATH
+
+    ## The maximum number of pending signals
+    echo "ulimit -i unlimited" | tee -a $PROF_PATH
+
+    ## The maximum size that may be locked into memory
+    echo "ulimit -l unlimited" | tee -a $PROF_PATH
+
+    ## The maximum memory size
+    echo "ulimit -m unlimited" | tee -a $PROF_PATH
+
+    ## The maximum number of open file descriptors
+    echo "ulimit -n 1048576" | tee -a $PROF_PATH
+
+    ## The maximum POSIX message queue size
+    echo "ulimit -q unlimited" | tee -a $PROF_PATH
+
+    ## The maximum stack size
+    echo "ulimit -s -H 65536" | tee -a $PROF_PATH
+    echo "ulimit -s 32768" | tee -a $PROF_PATH
+
+    ## The maximum number of seconds to be used by each process.
+    echo "ulimit -t unlimited" | tee -a $PROF_PATH
+
+    ## The maximum number of processes available to a single user
+    echo "ulimit -u unlimited" | tee -a $PROF_PATH
+
+    ## The maximum amount of virtual memory available to the process
+    echo "ulimit -v unlimited" | tee -a $PROF_PATH
+
+    ## The maximum number of file locks
+    echo "ulimit -x unlimited" | tee -a $PROF_PATH
+
+
+    echo 
+    green_msg 'System Limits are Optimized.'
+    echo 
+    sleep 0.5
+}
+
+
+# UFW Optimizations
+ufw_optimizations() {
+    echo
+    yellow_msg 'Installing & Optimizing UFW...'
+    echo 
+    sleep 0.5
+
+    ## Purge firewalld to install UFW.
+    sudo dnf -y remove firewalld
+
+    ## Install UFW if not installed.
+    dnf -y install epel-release
+    dnf -y up
+    dnf -y install ufw
+    
+    ## Disable UFW
+    sudo ufw disable
+
+    ## Open default ports.
+    sudo ufw allow $SSH_PORT
+    sudo ufw allow $SSH_PORT/udp
+    sudo ufw allow 80
+    sudo ufw allow 80/udp
+    sudo ufw allow 443
+    sudo ufw allow 443/udp
+    sleep 0.5
+
+    ## Change the UFW config to use System config.
+    sed -i 's+/etc/ufw/sysctl.conf+/etc/sysctl.conf+gI' /etc/default/ufw
+
+    ## Enable & Reload
+    echo "y" | sudo ufw enable
+    sudo ufw reload
+    echo 
+    green_msg 'UFW is Installed & Optimized. (Open your custom ports manually.)'
+    echo 
+    sleep 0.5
+}
+
+
+# Show the Menu
+show_menu() {
+    echo 
+    yellow_msg 'Choose One Option: '
+    echo 
+    green_msg '1  - Apply Everything. (RECOMMENDED)'
+    echo 
+    green_msg '2  - Complete Update + Make SWAP + Optimize Network, SSH & System Limits + UFW'
+    green_msg '3  - Complete Update + Make SWAP + Optimize Network, SSH & System Limits'
+    echo 
+    green_msg '4  - Complete Update & Clean the OS.'
+    green_msg '5  - Install Useful Packages.'
+    green_msg '6  - Make SWAP (2Gb).'
+    green_msg '7  - Optimize the Network, SSH & System Limits.'
+    echo 
+    green_msg '8  - Optimize the Network settings.'
+    green_msg '9  - Optimize the SSH settings.'
+    green_msg '10 - Optimize the System Limits.'
+    echo 
+    green_msg '11 - Install & Optimize UFW.'
+    echo 
+    red_msg 'q - Exit.'
+    echo 
+}
+
+
+# Choosing Program
+main() {
+    while true; do
+        show_menu
+        read -p 'Enter Your Choice: ' choice
+        case $choice in
+        1)
+            apply_everything
+
+            echo 
+            green_msg '========================='
+            green_msg  'Done.'
+            green_msg '========================='
+
+            ask_reboot
+            ;;
+        2)
+            complete_update
+            sleep 0.5
+
+            swap_maker
+            sleep 0.5
+
+            sysctl_optimizations
+            sleep 0.5
+
+            remove_old_ssh_conf
+            sleep 0.5
+    
+            update_sshd_conf
+            sleep 0.5
+
+            limits_optimizations
+            sleep 0.5
+
+            find_ssh_port
+            ufw_optimizations
+            sleep 0.5
+
+            echo 
+            green_msg '========================='
+            green_msg  'Done.'
+            green_msg '========================='
+
+            ask_reboot
+            ;;
+        3)
+            complete_update
+            sleep 0.5
+
+            swap_maker
+            sleep 0.5
+
+            sysctl_optimizations
+            sleep 0.5
+
+            remove_old_ssh_conf
+            sleep 0.5
+    
+            update_sshd_conf
+            sleep 0.5
+
+            limits_optimizations
+            sleep 0.5
+
+            echo 
+            green_msg '========================='
+            green_msg  'Done.'
+            green_msg '========================='
+
+            ask_reboot
+            ;;
+        4)
+            complete_update
+            sleep 0.5
+
+            echo 
+            green_msg '========================='
+            green_msg  'Done.'
+            green_msg '========================='
+
+            ask_reboot
+            ;;
+            
+        5)
+            complete_update
+            installations
+            enable_packages
+            sleep 0.5
+
+            echo 
+            green_msg '========================='
+            green_msg  'Done.'
+            green_msg '========================='
+
+            ask_reboot
+            ;;
+        6)
+            swap_maker
+            sleep 0.5
+
+            echo 
+            green_msg '========================='
+            green_msg  'Done.'
+            green_msg '========================='
+
+            ask_reboot
+            ;;
+        7)
+            sysctl_optimizations
+            sleep 0.5
+
+            remove_old_ssh_conf
+            sleep 0.5
+    
+            update_sshd_conf
+            sleep 0.5
+
+            limits_optimizations
+            sleep 0.5
+
+            echo 
+            green_msg '========================='
+            green_msg  'Done.'
+            green_msg '========================='
+
+            ask_reboot
+            ;;
+        8)
+            sysctl_optimizations
+            sleep 0.5
+
+            echo 
+            green_msg '========================='
+            green_msg  'Done.'
+            green_msg '========================='
+
+            ;;
+        9)
+            remove_old_ssh_conf
+            sleep 0.5
+    
+            update_sshd_conf
+            sleep 0.5
+
+            echo 
+            green_msg '========================='
+            green_msg  'Done.'
+            green_msg '========================='
+
+            ;;
+        10)
+            limits_optimizations
+            sleep 0.5
+
+            echo 
+            green_msg '========================='
+            green_msg  'Done.'
+            green_msg '========================='
+
+            ask_reboot
+            ;;
+        11)
+            find_ssh_port
+            ufw_optimizations
+            sleep 0.5
+
+            echo 
+            green_msg '========================='
+            green_msg  'Done.'
+            green_msg '========================='
+
+            ask_reboot
+            ;;
+        q)
+            exit 0
+            ;;
+
+        *)
+            red_msg 'Wrong input!'
+            ;;
+        esac
+    done
+}
+
+
+# Apply Everything
+apply_everything() {
+
+    complete_update
+    sleep 0.5
+
+    installations
+    sleep 0.5
+
+    enable_packages
+    sleep 0.5
+
+    swap_maker
+    sleep 0.5
+
+    sysctl_optimizations
+    sleep 0.5
+
+    remove_old_ssh_conf
+    sleep 0.5
+    
+    update_sshd_conf
+    sleep 0.5
+
+    limits_optimizations
+    sleep 0.5
+
+    find_ssh_port
+    ufw_optimizations
+    sleep 0.5
+}
+
+
+main
